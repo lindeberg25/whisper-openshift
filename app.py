f09361907f77e7611f1c6d359f56b99aff31ebd1
lindeberg25 <lindpessoa@gmail.com>
2023-02-01 12:43:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
-from flask import Flask, abort, request
-from tempfile import NamedTemporaryFile
-import whisper
-import torch
 
-# Check if NVIDIA GPU is available
-#torch.cuda.is_available()
-#DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-
-# Load the Whisper model:
-model = whisper.load_model("base", device=DEVICE)
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello():
-    return "Whisper Hello World!"
+def index():
+    return render_template('index.html', message="Hello World..!!")
 
-
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
